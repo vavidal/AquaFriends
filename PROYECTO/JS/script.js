27375ef33aa16369e9cc15fccdbab0b9822dfed1
nay.cardenas <nay.cardenas@duocuc.cl>
2025-08-14 15:09:48 -0400

    ELIMINAR

--- PROYECTO/JS/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-document.querySelector('.back-to-top').addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
